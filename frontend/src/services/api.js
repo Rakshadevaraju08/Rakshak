82acdeb71ab31c8d86a3da7dc2f70643a1ec5f87
Yamini Kashyap <ysjceengg2006@gmail.com>
2026-09-13 00:30:15 +0530

    RescueGrid control room updates

--- frontend/src/services/api.js
+++
@@ -1,8 +0,0 @@
-export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001/api';
-
-export const apiFetch = (path, options = {}) => {
-  const token = localStorage.getItem('disasterlink_token');
-  const headers = new Headers(options.headers || {});
-  if (token) headers.set('Authorization', `Bearer ${token}`);
-  return fetch(`${API_URL}${path}`, { ...options, headers });
-};
